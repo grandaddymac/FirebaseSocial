af69303df9a20f9b36388f91b693a2b693af7dc1
gdm <gdm.mbp@gdmMBP.local>
2018-12-30 18:10:16 -0500

    Add Firebase authentication for Google logins. Add logout functionality for Google.

--- FirebaseSocial/ViewController.swift
+++ FirebaseSocial/ViewController.swift
@@ -21,12 +21,62 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
     GIDSignIn.sharedInstance()?.uiDelegate = self
     
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user == nil {
+                self.userInfoLabel.text = "No user logged in"
+            } else {
+                self.userInfoLabel.text = "Welcome user: \(user?.uid ?? "")"
+            }
+        }
+    }
 
     @IBAction func googleSignInTapped(_ sender: Any) {
         GIDSignIn.sharedInstance()?.signIn()
     }
     
-    @IBAction func logoutTapped(_ sender: Any) {
-    }
-}
+    @IBAction func customGoogleTapped(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
 
+    }
+
+
+    @IBAction func logoutTapped(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            logoutSocial()
+            try firebaseAuth.signOut()
+        } catch let signoutError as NSError {
+            debugPrint("Error signing out", signoutError)
+        }
+    }
+    
+    func firebaseLogin(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                return
+            }
+        }
+    }
+    
+    func logoutSocial() {
+        guard let user = Auth.auth().currentUser else { return }
+        for info in (user.providerData) {
+            switch info.providerID {
+            case GoogleAuthProviderID:
+                GIDSignIn.sharedInstance()?.signOut()
+                print("google")
+            case TwitterAuthProviderID:
+                print("twitter")
+            case FacebookAuthProviderID:
+                print("facebook")
+            default:
+                break
+            }
+        }
+    
+    }
+
+}
